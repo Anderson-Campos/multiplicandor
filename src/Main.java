a9235cf4a02dbb1b6d0a05fb04b1b18358db6038
Anderson Campos <andersonbcamposbr@gmail.com>
2024-10-26 12:15:38 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número para verificar a tabuada: ");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            int soma = num * i;
+            System.out.println(num + " + " + i + " = " + soma);
+        }
+
+    }
+}
